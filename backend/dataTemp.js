2f098c9ae37567e805703faf0e58d24effa7731f
SURAJ <surajjadhav00787@gmail.com>
2023-08-22 20:07:10 +0530

    update in code that give error

--- backend/dataTemp.js
+++ backend/dataTemp.js
@@ -66,12 +66,10 @@ const contactTemp = new mongoose.Schema({
 const loginSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
         unique: true
     },
     password: {
         type: String,
-        required: true
     }
 })
 
