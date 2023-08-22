3e7db73c513692aa5047d594f53b73379d008083
SURAJ <surajjadhav00787@gmail.com>
2023-08-22 19:51:10 +0530

    some changes in Database Code

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_CONNECT_URL).then(() => console.log("DB Connected")).catch((err) => console.log(err));
+// mongoose.connect(process.env.MONGODB_CONNECT_URL).then(() => console.log("DB Connected")).catch((err) => console.log(err.message));
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://thefitnessclub:9mkNKI9Rq7oACKBC@thefitnessclub .otrkdta.mongodb.net/?retryWrites=true&w=majority");
+        console.log("DB Connected");
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+module.exports = connectDB;
