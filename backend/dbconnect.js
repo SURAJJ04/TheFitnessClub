7ec55c9f1f1f45f9baf64ff12e490bdf0edeaa4a
SURAJ <surajjadhav00787@gmail.com>
2023-08-22 19:28:07 +0530

    Change in mongodb server

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -1,7 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/The_Fitness_Club", {
-    usenewurlparser: true,
-    useunifiedtopology: true,
-    family: 4
-}).then(() => console.log("DB Connected")).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGODB_CONNECT_URL).then(() => console.log("DB Connected")).catch((err) => console.log(err));
